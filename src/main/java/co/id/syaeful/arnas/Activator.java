bff7128ebb97e254692688b0b7679657a6f76029
Syaefulloh Arnas <syaefulloh.arnas@itasoft.co.id>
2023-10-31 09:32:23 +0700

    first commit

---
+++ src/main/java/co/id/syaeful/arnas/Activator.java
@@ -0,0 +1,25 @@
+package co.id.syaeful.arnas;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class Activator implements BundleActivator {
+
+    protected Collection<ServiceRegistration> registrationList;
+
+    public void start(BundleContext context) {
+        registrationList = new ArrayList<ServiceRegistration>();
+
+        //Register plugin here
+//        registrationList.add(context.registerService(JasperTest.class.getName(), new JasperTest(), null));
+    }
+
+    public void stop(BundleContext context) {
+        for (ServiceRegistration registration : registrationList) {
+            registration.unregister();
+        }
+    }
+}
